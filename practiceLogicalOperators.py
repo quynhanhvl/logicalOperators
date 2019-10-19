aa4e62c5b37a71a43b019fdf5b59cbd5b9ac383c
quynhanhvl <quynhanhvl@users.noreply.github.com>
2019-10-19 14:10:25 -0500

    Initial commit

---
+++ practiceLogicalOperators.py
@@ -0,0 +1,14 @@
+'''
+Created on Oct 5, 2019
+
+@author: ITAUser
+'''
+from builtins import True
+a = False 
+b = 20>9
+c = 1==1
+d=True
+e=True
+print(a and (b and c) or (d and e))
+print ( a and b and c)
+print(4==4 and (6==6 or 2<5) and 25<9)
